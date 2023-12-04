e63b52f10d8b122d29eb00504e6e1a378e3ff9d1
rhossain5420 <150310559+rhossain5420@users.noreply.github.com>
2023-12-04 11:39:10 -0500

    This is our FP2 assignment HW

---
+++ MealsResponse.java
@@ -0,0 +1,21 @@
+package sample;
+
+import java.util.List;
+
+public class MealsResponse {
+
+     private List<Meal> meals;
+     
+    //  public MealsResponse(List<Meal> meals) {
+//         this.meals = meals;
+//      }
+
+    public List<Meal> getMeals() {
+        return meals;
+    }
+
+    public void setMeals(List<Meal> meals) {
+        this.meals = meals;
+    }
+    
+}
